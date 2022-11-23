bdd5c9d317a38ded7a6d3a03b72e98050d688849
Govind Banura <govindbanura2310@gmail.com>
2022-11-23 10:43:27 +0530

    Update titanic-classification-regression_train.py

--- main/titanic-classification-regression_train.py
+++ main/titanic-classification-regression_train.py
@@ -4,6 +4,9 @@ import numpy as np
 from compress_pickle import dump
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 import os
 
 
@@ -12,44 +15,29 @@ try:
 except Exception as e:
     print(e)
 
-
-def impute_age(cols):
-    Age = cols[0]
-    Pclass = cols[1]
-    if pd.isnull(Age):
-        if Pclass == 1:
-            return 37
-        elif Pclass == 2:
-            return 29
-        else:
-            return 24
-    else:
-        return Age
-
-
-train['Age'] = train[['Age','Pclass']].apply(impute_age,axis=1)
-train['Embarked'] = train['Embarked'].fillna('S')
-train.drop('Cabin',axis=1,inplace=True)
-train.dropna(inplace=True)
-sex = pd.get_dummies(train['Sex'],drop_first=True)
-embark = pd.get_dummies(train['Embarked'],drop_first=True)
-train.drop(['Sex','Embarked','Name','Ticket'],axis=1,inplace=True)
-train = pd.concat([train,sex,embark],axis=1)
-
-X_train, X_test, y_train, y_test = train_test_split(train.drop(['Survived'],axis=1), 
-                                                    train['Survived'], test_size=0.10, 
-                                                    random_state=101)
+train_df, target = train.drop(['Survived'],axis=1), train['Survived']
 print("split done")
 
-rf= RandomForestClassifier(n_estimators=500)
-rf.fit(X_train,y_train)
+features_categoricas = ['Embarked', 'Sex', 'Pclass']
+features_para_remover = ['Name', 'Cabin', 'Ticket', 'PassengerId']
+
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder())
+])
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('Features categoricas', categorical_transformer, features_categoricas),
+        ('Feature para remover', 'drop', features_para_remover)
+])
+
+
+model = Pipeline([('preprocessor', preprocessor),
+                 ('clf', RandomForestClassifier(n_estimators=500)),
+                 ])
+
+model.fit(train_df, target)
+
 
 print("creating pkl file")
 
-print(os.getcwd())
-
-dump(rf,'./model_1.pkl',compression="lzma",set_default_extension=False)
-
-rf_pre=rf.predict(X_test)
-
-print(rf_pre)
+dump(model,'./model_1.pkl',compression="lzma",set_default_extension=False)
